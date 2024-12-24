70df1b1e8d38e4111623e0af58ce34ce8d429149
virat-18-git <ksprddy6@gmail.com>
2024-12-24 13:00:12 +0530

    while loop

---
+++ statements.py
@@ -0,0 +1,44 @@
+list=[1,55,66,77,54,99,38]
+for i in list:
+    if i % 2==0:
+        print(i,"even")
+    else:
+        print(i,"odd")
+
+
+
+# # while condition:
+#     print('')
+
+# i=0
+# while i<30:
+#     print(i)
+# if i%2==0:
+#     print(i)
+ 
+#   i +=2
+#   print(i)
+
+# i=-10
+while i<=20:
+#    i=i+2
+   if i>0:
+     print(i,"positive")
+   elif i ==0:
+     print("zero")
+   else:
+     print(i,"negative")
+   i=i+1
+
+
+# jump statements
+for i in range(0,50):
+  print(i)
+  if i==20:
+    break
+  print(i)
+
+for i in range(0,50):
+    print(i)
+    break
+   
